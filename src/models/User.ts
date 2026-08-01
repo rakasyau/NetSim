839e6089d2f015973b165a507a8b4628b057fbd2
Raka Syauqi <rakasyauqiferdian@gmail.com>
2026-08-01 21:04:18 +0700

    fix: hapus deklarasi index duplikat di User (unique email)

--- src/models/User.ts
+++ src/models/User.ts
@@ -15,7 +15,6 @@ const UserSchema = new Schema(
     email: {
       type: String,
       required: true,
-      unique: true,
       lowercase: true,
       trim: true,
       match: [/^\S+@\S+\.\S+$/, 'Format email tidak valid'],
